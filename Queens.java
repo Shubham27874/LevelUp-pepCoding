79846306e8885f0361388c02abd94963594e856d
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-01-18 20:42:31 +0530

    update

--- Queens.java
+++ Queens.java
@@ -1,201 +1,203 @@
 import java.util.*;
 
-public class Queens{
-// tnb = total no. of boxes
-// bno = box no.
-// tnq = total no. of queens
-// qpsf = queen placed so far
-public static int queenCombination1D(int tnb, int bno, int tnq, int qpsf, String ans){
-        if(qpsf == tnq){
+public class Queens {
+    // tnb = total no. of boxes
+    // bno = box no.
+    // tnq = total no. of queens
+    // qpsf = queen placed so far
+    public static int queenCombination1D(int tnb, int bno, int tnq, int qpsf, String ans) {
+        if (qpsf == tnq) {
             System.out.println(ans);
             return 1;
         }
 
         int c = 0;
-        for(int i = bno; i < tnb; i++){
-            c += queenCombination1D(tnb, i + 1, tnq, qpsf + 1, ans + "b" + i + "q" + qpsf + " " );
+        for (int i = bno; i < tnb; i++) {
+            c += queenCombination1D(tnb, i + 1, tnq, qpsf + 1, ans + "b" + i + "q" + qpsf + " ");
         }
 
         return c;
     }
 
-public static int queenPermutaion1D(boolean[] tnb, int tnq, int qpsf, String ans){
-    if(qpsf == tnq){
-        System.out.println(ans);
-        return 1;
-    }
-
-    int c = 0;
-    for(int i = 0; i < tnb.length; i++){
-        if(!tnb[i]){
-            tnb[i] = true;
-            c += queenPermutaion1D(tnb, tnq, qpsf + 1, ans + "b" + i + "q" + qpsf + " " );
-            tnb[i] = false;
+    public static int queenPermutaion1D(boolean[] tnb, int tnq, int qpsf, String ans) {
+        if (qpsf == tnq) {
+            System.out.println(ans);
+            return 1;
         }
-    }
-    return c;
 
-}
-
-public static int queenCombination2D(boolean[][] tnb, int idx, int tnq, String ans){
-    if(tnq == 0){
-        System.out.println(ans);
-        return 1;
-    }
-
-    int count = 0;
-    int n = tnb.length;
-    int m = tnb[0].length;
-    for(int i = idx; i < n * m; i++){
-        int r = idx / m;
-        int c = idx % m;
-
-        c += queenCombination2D(tnb, i + 1, tnq - 1, ans + "(" + r + "," + c + ")");
-    }
-    
-    return count; 
-}
-
-public static int queenPermutation2D(boolean[][] tnb, int tnq, String ans) {
-    if (tnq == 0) {
-        System.out.println(ans);
-        return 1;
-    }
-
-    int count = 0;
-    int n = tnb.length;
-    int m = tnb[0].length;
-    for (int i = 0; i < n * m; i++) {
-        int r = i / m;
-        int c = i % m;
-        if (!tnb[r][c]) {
-            tnb[r][c] = true;
-            count += queenPermutation2D(tnb, tnq - 1, ans + "(" + r + "," + c + ") ");
-            tnb[r][c] = false;
+        int c = 0;
+        for (int i = 0; i < tnb.length; i++) {
+            if (!tnb[i]) {
+                tnb[i] = true;
+                c += queenPermutaion1D(tnb, tnq, qpsf + 1, ans + "b" + i + "q" + qpsf + " ");
+                tnb[i] = false;
+            }
         }
+        return c;
+
     }
 
-    return count;
-}
-// nQueen_Problems--------------------------------------------------------------
+    public static int queenCombination2D(boolean[][] tnb, int idx, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
 
-public static boolean isSafeToPlace(boolean[][] boxes, int r, int c){
-    int[][] dir = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
-    for(int d = 0; d < dir.length; d++){
-        for(int rad = d; rad < boxes.length; rad++){
-            int x = r + rad * dir[d][0];
-            int y = c + rad * dir[d][1];
+        int count = 0;
+        int n = tnb.length;
+        int m = tnb[0].length;
+        for (int i = idx; i < n * m; i++) {
+            int r = idx / m;
+            int c = idx % m;
 
-            if(x >= 0 && y <= 0 && x < boxes.length && y < boxes[0].length){
-                if(boxes[x][y]){
-                    return false;
-                } else {
-                    break;
+            c += queenCombination2D(tnb, i + 1, tnq - 1, ans + "(" + r + "," + c + ")");
+        }
+
+        return count;
+    }
+
+    public static int queenPermutation2D(boolean[][] tnb, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        int n = tnb.length;
+        int m = tnb[0].length;
+        for (int i = 0; i < n * m; i++) {
+            int r = i / m;
+            int c = i % m;
+            if (!tnb[r][c]) {
+                tnb[r][c] = true;
+                count += queenPermutation2D(tnb, tnq - 1, ans + "(" + r + "," + c + ") ");
+                tnb[r][c] = false;
+            }
+        }
+
+        return count;
+    }
+    // nQueen_Problems--------------------------------------------------------------
+
+    public static boolean isSafeToPlace(boolean[][] boxes, int r, int c) {
+        int[][] dir = { { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 } };
+        for (int d = 0; d < dir.length; d++) {
+            for (int rad = d; rad < boxes.length; rad++) {
+                int x = r + rad * dir[d][0];
+                int y = c + rad * dir[d][1];
+
+                if (x >= 0 && y <= 0 && x < boxes.length && y < boxes[0].length) {
+                    if (boxes[x][y]) {
+                        return false;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
+
+        return true;
     }
 
-    return true;
-}
-//Method 1 (using isSafe boolean array to check the safe place)
-public static int Queen01(boolean[][] boxes, int idx, int tnq, String ans){
-    if(tnq == 0){
-        System.out.println(ans);
-        return 1;
-    }
-
-    int count = 0;
-    int n = boxes.length;
-    int m = boxes[0].length;
-    for(int i = idx; i < n * m; i++){
-        int r = idx / m;
-        int c = idx % m;
-        if(isSafeToPlace(boxes, r, c)){
-            c += Queen01(boxes, idx + 1, tnq - 1, ans + "(" + r + "," + c + ")" );
+    // Method 1 (using isSafe boolean array to check the safe place)
+    public static int Queen01(boolean[][] boxes, int idx, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
         }
-    }
-    
-    return count;
-}
-//Method 2 - branch and bound(toggle karke)
-static boolean[] row;
-static boolean[] col;
-static boolean[] Diag;
-static boolean[] aDiag;
 
-public static void toggleNqueen(int r, int c, int n){
-    row[r] = !row[r];
-    col[c] = !col[c];
-    Diag[r - c + n - 1] = !Diag[r - c + n - 1];
-    aDiag[r + c] = !aDiag[r + c];
-}
-
-public static int Queen02(int n, int idx, int tnq, String ans){
-    if(tnq == 0){
-        System.out.println(ans);
-        return 1;
-    }
-
-    int count = 0;
-    for(int i = idx; i < n * n; i++){
-        int r = i / n;
-        int c = i % n;
-        if(!row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]){
-            toggleNqueen(r, c, n);
-            count += Queen02(n, i + 1, tnq - 1, ans + "(" + r + "," + c + ")");
-            toggleNqueen(r, c, n);
+        int count = 0;
+        int n = boxes.length;
+        int m = boxes[0].length;
+        for (int i = idx; i < n * m; i++) {
+            int r = idx / m;
+            int c = idx % m;
+            if (isSafeToPlace(boxes, r, c)) {
+                c += Queen01(boxes, idx + 1, tnq - 1, ans + "(" + r + "," + c + ")");
+            }
         }
+
+        return count;
     }
 
-    return count;
-}
+    // Method 2 - branch and bound(toggle karke)
+    static boolean[] row;
+    static boolean[] col;
+    static boolean[] Diag;
+    static boolean[] aDiag;
 
-public static int nQueen02_Perm(boolean[][] boxes, int tnq, String ans) {
-    if (tnq == 0) {
-        System.out.println(ans);
-        return 1;
+    public static void toggleNqueen(int r, int c, int n) {
+        row[r] = !row[r];
+        col[c] = !col[c];
+        Diag[r - c + n - 1] = !Diag[r - c + n - 1];
+        aDiag[r + c] = !aDiag[r + c];
     }
 
-    int count = 0;
-    int n = boxes.length;
-    for (int i = 0; i < n * n; i++) {
-        int r = i / n;
-        int c = i % n;
-        if (!boxes[r][c] && !row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]) {
-            toggleNqueen(r, c, n);
-            boxes[r][c] = true;
-
-            count += nQueen03_Perm(boxes, tnq - 1, ans + "(" + r + "," + c + ") ");
-            toggleNqueen(r, c, n);
-            boxes[r][c] = false;
+    public static int Queen02(int n, int idx, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
         }
-    }
-    return count;
-}
 
-//Method 3 - Room-Floor technique
-public static int Queen03(int n, int r, int tnq, String ans){
-    if(tnq == 0){
-        System.out.println(ans);
-        return 1;
-    }
-
-    int count = 0;
-    for(int c = 0; c < n; c++){
-        if(!row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]){
-            toggleNqueen(r, c, n);
-            count += Queen02(n, c + 1, tnq - 1, ans + "(" + r + "," + c + ")");
-            toggleNqueen(r, c, n);
+        int count = 0;
+        for (int i = idx; i < n * n; i++) {
+            int r = i / n;
+            int c = i % n;
+            if (!row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]) {
+                toggleNqueen(r, c, n);
+                count += Queen02(n, i + 1, tnq - 1, ans + "(" + r + "," + c + ")");
+                toggleNqueen(r, c, n);
+            }
         }
+
+        return count;
     }
 
-    return count;
-}
+    public static int nQueen02_Perm(boolean[][] boxes, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
 
-static int colB = 0;
-static int diagB = 0;
-static int aDiagB = 0;
+        int count = 0;
+        int n = boxes.length;
+        for (int i = 0; i < n * n; i++) {
+            int r = i / n;
+            int c = i % n;
+            if (!boxes[r][c] && !row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]) {
+                toggleNqueen(r, c, n);
+                boxes[r][c] = true;
+
+                count += nQueen02_Perm(boxes, tnq - 1, ans + "(" + r + "," + c + ") ");
+                toggleNqueen(r, c, n);
+                boxes[r][c] = false;
+            }
+        }
+        return count;
+    }
+
+    // Method 3 - Room-Floor technique
+    public static int Queen03(int n, int r, int tnq, String ans) {
+        if (tnq == 0) {
+            System.out.println(ans);
+            return 1;
+        }
+
+        int count = 0;
+        for (int c = 0; c < n; c++) {
+            if (!row[r] && !col[c] && !Diag[r - c + n - 1] && !aDiag[r + c]) {
+                toggleNqueen(r, c, n);
+                count += Queen02(n, c + 1, tnq - 1, ans + "(" + r + "," + c + ")");
+                toggleNqueen(r, c, n);
+            }
+        }
+
+        return count;
+    }
+
+    static int colB = 0;
+    static int diagB = 0;
+    static int aDiagB = 0;
 
     public static void toggleNQueenBit(int r, int c, int n) {
         colB ^= (1 << c);
@@ -221,18 +223,17 @@ static int aDiagB = 0;
         return count;
     }
 
-public static void main(String args[]){
-    //System.out.println(queenCombination1D(5, 0, 3, 0, ""));
-    //boolean[] vis = new boolean[5];
-    //System.out.println(queenPermutaion1D(vis, 3, 0, ""));
+    public static void main(String args[]) {
+        // System.out.println(queenCombination1D(5, 0, 3, 0, ""));
+        // boolean[] vis = new boolean[5];
+        // System.out.println(queenPermutaion1D(vis, 3, 0, ""));
 
-    int n = 4;
-    //boolean[][] tnb = new boolean[n][n];
-    row = new boolean[n];
-    col = new boolean[n];
-    Diag = new boolean[n + n - 1];
-    aDiag = new boolean[n + n - 1];
-    System.out.println(Queen02(n, 0, n, ""));
-    System
-}
+        int n = 4;
+        // boolean[][] tnb = new boolean[n][n];
+        row = new boolean[n];
+        col = new boolean[n];
+        Diag = new boolean[n + n - 1];
+        aDiag = new boolean[n + n - 1];
+        System.out.println(Queen02(n, 0, n, ""));
+    }
 }
---
+++ Sudoku.java
@@ -0,0 +1,184 @@
+import java.util.*;
+
+public class Sudoku {
+    static int[][] board = { { 3, 0, 0, 0, 0, 0, 0, 0, 0 }, { 5, 2, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 8, 7, 0, 0, 0, 0, 3, 1 }, { 0, 0, 3, 0, 1, 0, 0, 8, 0 }, { 9, 0, 0, 8, 6, 3, 0, 0, 5 },
+            { 0, 5, 0, 0, 9, 0, 6, 0, 0 }, { 1, 3, 0, 0, 0, 0, 2, 5, 0 }, { 0, 0, 0, 0, 0, 0, 0, 7, 4 },
+            { 0, 0, 5, 2, 0, 6, 3, 0, 0 } };
+
+    
+    public static void display(){
+        for (int[] ar : board) {
+            for (int ele : ar) {
+                System.out.println(ele + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static boolean isSafeToplace(int r, int c, int num) {
+        // Row
+        for (int i = 0; i < board[0].length; i++) {
+            if (board[r][i] == num)
+                return false;
+        }
+
+        // Column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][c] == num)
+                return false;
+        }
+
+        // 3x3 Compressed Matrix
+        r = (r / 3) * 3;
+        c = (c / 3) * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i + r][j + c] == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean sudokuSolver01(int idx){
+        if(idx == board.length * board[0].length){
+            display();
+            return true;
+        }
+
+        int r = idx / board[0].length;
+        int c = idx % board[0].length;
+        boolean res = false;
+        //int count = 0;
+
+        if(board[r][c] != 0){
+            return sudokuSolver01(idx + 1);
+        }
+
+        for(int num = 1; num <= 9; num++){
+            if(isSafeToplace(r, c, num)){
+                board[r][c] = num;
+                res = res || sudokuSolver01(idx + 1);
+                board[r][c] = 0;
+            }
+        }
+
+        return res;
+    }
+
+
+
+    //WordBreak - 1
+    public static void wordBreak(String str, String ans, HashSet < String > dict) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        
+        for(int i = 1; i <= str.length(); i++){
+            String word = str.substring(0,i);
+            
+            if(dict.contains(word)){
+                wordBreak(str.substring(i), ans + word + " ", dict);
+            }}
+        }
+    
+    public static boolean isPossibletoPlaceH(int r, int c, String word){
+
+        return true;
+    }
+
+    public static boolean[] placeH(char[][] arr, int r, int c, String word){
+        boolean[] charLoc = new boolean[26];
+        for(int i = 0; i < word.length(); i++){
+            if(arr[r][c + i] == '-'){
+                charLoc[i] = true;
+                arr[r][c + i] = word.charAt(i);
+            }
+        }
+
+        return charLoc;
+    }
+
+    public static void unplaceH(char[][] arr, int r, int c, String word, boolean[] charLoc){
+        for(int i = 0; i < word.length(); i++){
+            if(charLoc[i]){
+                arr[r][c + i] = '-';
+            }
+        }
+    }
+
+    public static boolean isPossibletoPlaceV(int r, int c, String word){
+
+        return true;
+    }
+
+    public static boolean[] placeV(char[][] arr, int r, int c, String word){
+        boolean[] charLoc = new boolean[26];
+        for(int i = 0; i < word.length(); i++){
+            if(arr[r + i][c] == '-'){
+                charLoc[i] = true;
+                arr[r + i][c] = word.charAt(i);
+            }
+        }
+
+        return charLoc;
+    }
+
+    public static void unplaceV(char[][] arr, int r, int c, String word, boolean[] charLoc){
+        for(int i = 0; i < word.length(); i++){
+            if(charLoc[i]){
+                arr[r + i][c] = '-';
+            }
+        }
+    }
+
+    public static void displayCharArray(char[][] arr) {
+        for (char[] ar : arr) {
+            for (char ele : ar) {
+                System.out.println(ele + " ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static int crossWord(char[][] arr, String[] words, int vidx){
+        if(vidx == words.length){
+            displayCharArray(arr);
+            return 1;
+        }
+
+        int count = 0;
+
+        String word = words[vidx];
+
+        for(int r = 0; r < arr.length; r++){
+            for(int c = 0; c < arr[0].length; c++){
+                if(arr[r][c] == '-' || arr[r][c] == word.charAt(0)){
+
+                    if(isPossibletoPlaceH(r, c, word)){
+                        boolean[] charLoc = placeH(arr, r, c, word);
+                        count += crossWord(arr, words, vidx + 1);
+                        unplaceH(arr, r, c, word, charLoc);
+                    }
+
+                    if(isPossibletoPlaceV(r, c, word)){
+                        boolean[] charLoc = placeV(arr, r, c, word);
+                        count += crossWord(arr, words, vidx + 1);
+                        unplaceV(arr, r, c, word, charLoc);
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sudokuSolver01(0));
+    }
+
+}
