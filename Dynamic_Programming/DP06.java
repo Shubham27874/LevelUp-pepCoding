0b472a7819fb6b3c48156985fbd6370b16ff5250
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-04-04 15:50:41 +0530

    update

--- Dynamic_Programming/DP06.java
+++ Dynamic_Programming/DP06.java
@@ -121,6 +121,60 @@ public class DP06 {
         return maxIncrSum;
     }
 
+    //LIS set
+
+    //LIS_LR
+    public static int[] LIS_LR(int[] arr){
+        int n = arr.length;
+        int[] dp = new int[n];
+        int len = 0;
+
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
+            for(int j = i - 1; j >= 0; j--){
+                if(arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            len = Math.max(len, dp[i]);
+        }
+
+        return dp;
+    }
+
+    //LDS_LR
+    public static int[] LIS_RL(int[] arr){
+        int n = arr.length;
+        int[] dp = new int[n];
+        int len = 0;
+
+        for(int i = n - 1; i >= 0; i--){
+            dp[i] = 1;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+
+            len = Math.max(len, dp[i]);
+        }
+
+        return dp;
+    }
+
+    //LBS_upHill
+    public static int LongestBitonicSequence(int[] nums) {
+        int[] LIS = LIS_LR(nums);
+        int[] LDS = LIS_RL(nums);
+
+        int len = 0;
+        for(int i = 0; i < arr.length; i++) {
+            len = Math.max(len, LIS[i] + LDS[i] - 1);
+        }
+
+        return len;
+    }
 
 
 }
--- Dynamic_Programming/DP07.java
+++ Dynamic_Programming/DP07.java
@@ -1,3 +1,7 @@
+import java.lang.*;
+
 public class DP07 {
-    
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
 }
