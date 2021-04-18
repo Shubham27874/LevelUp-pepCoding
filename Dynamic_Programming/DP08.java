a0af504ddf8e1678c61af80254dd0cbd6897c522
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-04-18 19:48:56 +0530

    update

--- Dynamic_Programming/DP08.java
+++ Dynamic_Programming/DP08.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class DP08 {
     public static void print1D(int[] arr) {
         for (int ele : arr) {
@@ -32,6 +34,7 @@ public class DP08 {
         return dp[tar] = count;
     }
 
+    //Leetcode 377
     public static int coinChangePermutation_DP(int[] arr, int Tar, int[] dp){
         dp[0] = 1; /*if(tar == 0){
                      return dp[tar] = 1;
@@ -48,13 +51,80 @@ public class DP08 {
         return dp[Tar];
     }
 
-    public static void coinChangeCombination(int[] arr, int amt){
+    public static int coinChangeCombination_memo(int[] arr, int tar, int li, int[][] dp){
+        if(tar == 0){
+            return dp[li][tar] = 1;
+        }
+
+        int count = 0;
+        for(int i = li; i >= 0; i--)
+            if(tar - arr[i] >= 0){
+                count += coinChangeCombination_memo(arr, tar - arr[i], i, dp);
+            }
+
+        return dp[li][tar] = count;
+    }
+
+    public static int coinChangeCombination_2DP(int[] arr, int Tar, int Li, int[][] dp){
+        for(int li = 0; li <= Li; li++){
+            for(int tar = 0; tar <= Tar; tar++){
+                if(tar == 0){
+                    dp[li][tar] = 1;
+                    continue;
+                }
+
+                for(int i = li; i >= 0; i--)
+                    if(tar - arr[i] >= 0){
+                        dp[li][tar] += dp[i][tar - arr[i]];
+                    }
+            }
+        }
         
+        return dp[Li][Tar];
     }
 
-    public static void coinChange(int[] arr, int amt){
+    //Leetcode 518
+    public static int coinChangeCombination_1DP(int[] arr, int Tar, int[] dp){
+        dp[0] = 1; 
+        for(int ele : arr) {
+        for(int tar = ele; tar <= Tar; tar++){
+                if(tar - ele >= 0){
+                    dp[tar] += dp[tar - ele]; 
+                }
+            }
+        }
 
+        return dp[Tar];
     }
+
+    //Leetcode 322
+    public int coinChange(int[] coins, int amount, int[] dp) {
+        if(amount == 0){
+            return 0;
+        }
+
+        if(dp[amount] != (int)1e9)
+            return dp[amount];
+
+        int minCoins = (int)1e8;
+        for(int ele : coins){
+            if(amount - ele >= 0){
+                minCoins = Math.min(minCoins, coinChange(coins, amount - ele, dp) + 1);
+            }
+        }
+
+        return dp[amount] = minCoins;
+    }
+
+    public int coinChange(int[] coins, int amount){
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, (int)1e9);
+
+        int ans = coinChange(coins, amount, dp);
+
+        return ans != (int)1e8 ? ans : -1;
+    }
+
 
     public static void main(String[] args){
         
