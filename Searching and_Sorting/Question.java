474e45deb29ee7ad3c778159ab32ea321f90c65e
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-06-25 16:46:18 +0530

    update

--- Searching and_Sorting/Question.java
+++ Searching and_Sorting/Question.java
@@ -261,4 +261,38 @@ public class Question {
 
         return false;
     }
+
+    //Leetcode 153
+    public int findMin(int[] nums) {
+        int n = nums.length, si = 0, ei = n - 1;
+        
+        while(si < ei){
+            int mid = (si + ei) / 2;
+            if(nums[mid] < nums[ei])
+                ei = mid;
+            else if(nums[si] <= nums[mid])
+                si = mid + 1;
+        }
+
+        return nums[si];
+    }
+
+    //Leetcode 154
+    public int findMin(int[] nums) {
+        int n = nums.length, si = 0, ei = n - 1;
+        if(nums[si] < nums[ei])
+            return nums[si];
+
+        while(si < ei){
+            int mid = (si + ei) / 2;
+            if(nums[mid] < nums[ei])
+                ei = mid;
+            else if(nums[mid] > nums[ei])
+                si = mid + 1;
+            else 
+                ei--;
+        }
+
+        return nums[si];
+    }
 }
