d5605959d0a2c5edc1593ffe55186833c0b18a1a
Shubham27874 <60144043+Shubham27874@users.noreply.github.com>
2021-07-07 12:21:42 +0530

    update

--- Tree/BST.java
+++ Tree/BST.java
@@ -150,4 +150,37 @@ public class BST {
 
         return node;
     }
+
+    public static void predSuccInBST(TreeNode node, int data){
+        TreeNode curr = node;
+        TreeNode pred = null;
+        TreeNode succ = null;
+        boolean isDataPresent = false;
+
+        while(curr != null){
+            if(curr.val == data){
+                isDataPresent = true;
+                if(curr.left != null) {
+                    pred = curr.left;
+                    while(pred.right != null)
+                        pred = pred.left;
+                }
+
+
+                if(curr.right != null) {
+                    succ = curr.right;
+                    while(succ.left != null)
+                        succ = succ.left;
+                }
+
+                break;
+            } else if(curr.val < data){
+                pred = curr;
+                curr = curr.right;
+            } else {
+                succ = curr;
+                curr = curr.left;
+            }
+        }
+    }
 }
